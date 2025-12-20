85e88e299a763508ce43d2b2451958ae96f3cda5
[Neeraj kushwaha] <[workwithneeraj.01@gmail.com]>
2025-12-20 13:39:39 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32 64x64" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
